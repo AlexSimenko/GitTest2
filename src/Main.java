2ba9a07de964407895934f30277d71c63028b302
AlexSimenko <simenkoalex2@gmail.com>
2023-06-03 21:27:32 +0300

    New commit

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ import animal.Bird;
 public class Main {
     public static void main(String[] args) {
         Bird bird = new Bird("Sparrow","Small bird");
-        bird.Fly(true);
+        bird.Fly(false);
         System.out.println("this bird is "+bird.getName());
         System.out.println("classification "+ bird.getClassification());
+        System.out.println("Sometimes bird cannot fly");
     }
 }
