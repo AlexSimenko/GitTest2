37f7dcf7e5774474548210c832b9bd104f869ebb
alexanderik79 <alexanderik79@gmail.com>
2023-06-04 10:16:10 +0200

    мои изменения Пацаган 04.06.2023

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,13 @@ public class Main {
         Bird bird = new Bird("Sparrow","Small bird");
         bird.Fly(true);
         System.out.println("this bird is "+bird.getName());
+
+
+        //мои изменения Пацаган 04.06.2023
+        Bird bird1 = new Bird("Raven","Big bird");
+        bird1.Fly(true);
+        System.out.println("this bird is "+bird1.getName());
+
+
     }
 }
